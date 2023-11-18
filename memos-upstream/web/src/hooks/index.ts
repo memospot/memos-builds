daad8af9192ee9a81dacfd7d9d3f1eb216d9094f
lincolnthalles <lincolnthalles@users.noreply.github.com>
2023-11-18 00:41:01 -0300

    Remove memos-upstream subtree

--- memos-upstream/web/src/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./useDebounce";
-export * from "./useLoading";
-export * from "./useTimeoutFn";
-export * from "./useToggle";
--- memos-upstream/web/src/types/view.d.ts
+++
@@ -1,6 +0,0 @@
-interface DialogCallback {
-  destroy: FunctionType;
-  hide: FunctionType;
-}
-
-type DialogProps = DialogCallback;
