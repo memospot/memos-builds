9dd896e8e769e30df587a5b54ec80fccc69312aa
Lincoln Nogueira <lincolnthalles@users.noreply.github.com>
2024-01-15 10:30:19 -0300

    chore: update ci

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
-export TERM=xterm-256color
-reset=$(tput sgr0)
-green=$(tput setaf 2)
-magenta=$(tput setaf 5)
-
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
-echo $TZ > /etc/timezone
+# This custom entry point makes debugging image builds easier.
 
 MAIN=/opt/memos/memos
 
-arch=$(uname -m)
+set -eu
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+
+machinearch=$(uname -m)
 release=$(cat /etc/os-release | grep PRETTY_NAME | cut -d'"' -f2)
 platform=$(cat /opt/memos/buildinfo | grep TARGETPLATFORM | cut -d'=' -f2)
 checksum=$(sha256sum ${MAIN} | cut -d' ' -f1)
 bindate=$(stat -c %y ${MAIN} | cut -d'.' -f1)
+baseimage=$(echo $release | cut -d' ' -f1)
 
-echo "${magenta}Timezone: ${green}$TZ${reset}"
-echo "${magenta}Container base image: ${green}$release ($arch)${reset}"
-echo "${magenta}Container target platform: ${green}$platform${reset}"
-echo "${magenta}Main binary date: ${green}$bindate${reset}"
-echo "${magenta}Main binary checksum: ${green}$checksum${reset}"
+print () { echo -e $@ ; }
+if [ "${baseimage}" = "Debian" ]; then
+    print () { echo $@ ; }
+fi
 
-${MAIN}
+reset="\e\033[0m"
+green="\e[32m"
+magenta="\e[35m"
+
+print "${magenta}Timezone:          ${green}$TZ${reset}"
+print "${magenta}Base image:        ${green}$release${reset}"
+print "${magenta}Target platform:   ${green}$platform${reset}"
+print "${magenta}Host Architecture: ${green}$machinearch${reset}"
+echo ""
+print "${magenta}Main binary date:     ${green}$bindate${reset}"
+print "${magenta}Main binary checksum: ${green}$checksum${reset}"
+echo ""
+
+exec ${MAIN}
