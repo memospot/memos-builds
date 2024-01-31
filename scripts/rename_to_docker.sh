54bf722a42d2db7b9b37124d05dfdd649e32d43e
Lincoln Nogueira <lincolnthalles@users.noreply.github.com>
2024-01-31 18:09:56 -0300

    chore: fix rename script - add explicit subtree pulls to CI

--- scripts/rename_to_docker.sh
+++ scripts/rename_to_docker.sh
@@ -1,32 +1,34 @@
-reset="\e\033[0m"
-cyan="\e[36m"
-green="\e[32m"
+#!/usr/bin/env bash
+
+reset="\033[0m"
+cyan="\033[36m"
+green="\033[32m"
 
 set -eu
 
-echo -e "${cyan}Renaming builds to Docker format...${reset}"
+printf "${cyan}Renaming builds to Docker format...${reset}\n"
 for dir in ./build/backend/memos_linux_*; do
-    if [[ ! -d "${dir}" ]]; then
+    if [ ! -d "${dir}" ]; then
         continue
     fi
 
-    already_renamed=$(echo "${dir}" | grep -E "armv[5-7]$|amd64v[2-4]$|_386$|arm64$" || true)
-    if [[ -n "${already_renamed}" ]]; then
+    already_renamed=$(printf "${dir}" | grep -E "armv[5-7]$|amd64v[2-4]$|_386$|arm64$" || true)
+    if [ -n "${already_renamed}" ]; then
         continue
     fi
 
     new_name="${dir}"
-    new_name="${new_name//amd64_v1/amd64}"
-    new_name=$(echo "${new_name}" | sh -c 'sed -E "s/(amd64_v)([2-4])/amd64v\2/"')
-    new_name=$(echo "${new_name}" | sh -c 'sed -E "s/(arm_)([5-7])/armv\2/"')
+    new_name=$(printf "${new_name}" | sed 's/amd64_v1/amd64/g')
+    new_name=$(printf "${new_name}" | sed -E "s/(amd64_v)([2-4])/amd64v\2/")
+    new_name=$(printf "${new_name}" | sed -E "s/(arm_)([5-7])/armv\2/")
 
-    if [[ "${new_name}" == "${dir}" ]]; then
+    if [ "${new_name}" = "${dir}" ]; then
         continue
     fi
 
-    dir=${dir//\\//}
-    new_name=${new_name//\\//}
+    dir=$(echo "$dir" | sed 's/\\\\/\//g')
+    new_name=$(echo "$new_name" | sed 's/\\\\/\//g')
 
-    echo -e "${green}Renaming ${dir} to ${new_name}${reset}"
+    printf "${green}Renaming ${dir} to ${new_name}${reset}\n"
     mv -f "$dir" "$new_name"
 done
